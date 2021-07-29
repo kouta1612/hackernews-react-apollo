1685174b509dab874fe36c4f55694a4c1d620fb0
hagiwara-lvgs <kota.hagiwara@leverages.jp>
2021-07-29 13:06:43 +0900

    back

--- src/components/Header.js
+++ src/components/Header.js
@@ -11,18 +11,18 @@ const Header = () => {
       <div className="flex flex-fixed black">
         <div className="fw7 mr1">Hacker News</div>
         <Link to="/" className="ml1 no-underline black">
-          LIST
+          new
         </Link>
         <div className="ml1">|</div>
-        <Link to="/create" className="ml1 no-underline black">
-          NEW
+        <Link to="/top" className="ml1 no-underline black">
+          top
         </Link>
         <div className="ml1">|</div>
         <Link
-          to="/login"
+          to="/search"
           className="ml1 no-underline black"
         >
-          LOGIN
+          search
         </Link>
         {authToken && (
           <div className="flex">
